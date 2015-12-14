d29558f5025097e4cff7130c1e04eb60078c1a07
Ed Flanagan <edsync256@gmail.com>
2015-12-13 20:05:18 -0500

    Modify to use jQuery instead of vanilla JS
    * Add jQuery v2.1.4
    * Update manifest.json to use jQuery file in content_script
    * Transform remove.js code to use jQuery

--- remove.js
+++ remove.js
@@ -1,8 +1,4 @@
-document.addEventListener('DOMContentLoaded', function removeMoments() {
-    var moments = document.getElementsByClassName('moments js-moments-tab');
-
-    for (var i = 0; i < moments.length; i++) {
-        moments[i].parentNode.removeChild(moments[i]);
-    }
+$(document).ready(function removeMoments() {
+    $('.moments, .js-moments-tab').remove();
 });
 
